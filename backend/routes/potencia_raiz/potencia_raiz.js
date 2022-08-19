9128d2afe8ac78d2332d31ab60ad3ce2a816b52d
Jairo Ramírez <2987301380101@ingenieria.usac.edu.gt>
2022-08-19 01:13:27 -0600

    Se realizo un endpoint GET retorno de raiz cubica de un numero 201800712

--- backend/routes/potencia_raiz/potencia_raiz.js
+++ backend/routes/potencia_raiz/potencia_raiz.js
@@ -1,12 +1,21 @@
-module.exports = (express,app) => {
-	//RETORNA POTENCIA CUBICA
-app.get("/potencia/:NUMERO", (req, res) => {
+module.exports = (express, app) => {
+  //RETORNA POTENCIA CUBICA
+  app.get("/potencia/:NUMERO", (req, res) => {
     var numero = req.params.NUMERO;
     //calcular cubo
-    var resultado = Math.pow(numero,3);
+    var resultado = Math.pow(numero, 3);
 
     return res.status(200).send({
-        resultado:  resultado
+      resultado: resultado,
     });
-});
-}
+  });
+  app.get("/raiz/:NUMERO", (req, res) => {
+    var numero = req.params.NUMERO;
+    //calcular raiz
+    var resultado = Math.cbrt(numero);
+
+    return res.status(200).send({
+      resultado: resultado,
+    });
+  });
+};
